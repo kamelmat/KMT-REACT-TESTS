17399aa5563e5d52b2a796f1802c99cf8caf39f0
josh1999 <elibetancur18@gmail.com>
2025-03-19 19:32:37 -0700

    cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,82 +1,89 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import TemporizadorDeSegundos from "./TemporizadorDeSegundos"
+import TemporizadorDeSegundos from "./TemporizadorDeSegundos";
 import { faPlay, faPause, faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
 
-const Home = () => {
-	const [timerSeconds, setCount] = useState(0);
-	const [estaPausa, setEstaPausa] = useState(false);
-	const [estaAlertado, setEstaAlertado] = useState(false);
-	const [alertaEnEntrada, setAlertaEnEntrada] = useState("100");
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timerSeconds: 0,
+            estaPausa: false,
+            estaAlertado: false,
+            alertaEnEntrada: "100"
+        };
+        this.interval = setInterval(this.incrementarTemporizador, 1000);
+    }
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (!estaPausa) {
-				setCount(timerSeconds => timerSeconds + 1);
-			}
-		}, 1000);
-		
-		const alertaEnNumero = Number(alertaEnEntrada);
-		if (timerSeconds >= alertaEnNumero && !estaAlertado) {
-			setEstaAlertado(true);
-		} else if (timerSeconds < alertaEnNumero && estaAlertado) {
-			setEstaAlertado(false)
-		}
+    incrementarTemporizador = () => {
+        if (!this.state.estaPausa) {
+            this.setState(prevState => {
+                const nuevoTiempo = prevState.timerSeconds + 1;
+                const alertaEnNumero = Number(prevState.alertaEnEntrada);
+                return {
+                    timerSeconds: nuevoTiempo,
+                    estaAlertado: nuevoTiempo >= alertaEnNumero
+                };
+            });
+        }
+    };
 
-		return () => clearInterval(interval);
-	}, [estaPausa, timerSeconds, alertaEnEntrada, estaAlertado]);
+    manejarCambioEntrada = (event) => {
+        const nuevoValor = event.target.value;
+        this.setState(prevState => ({
+            alertaEnEntrada: nuevoValor,
+            estaAlertado: prevState.timerSeconds >= Number(nuevoValor)
+        }));
+    };
 
-	const manejarCambioEntrada = (event) => {
-		setAlertaEnEntrada(event.target.value);
-	};
+    alternarPausa = () => {
+        this.setState(prevState => ({ estaPausa: !prevState.estaPausa }));
+    };
 
-	const alternarPausa = () => {
-		setEstaPausa(!estaPausa);
-	};
+    temporizadorDeReinicio = () => {
+        this.setState({ timerSeconds: 0, estaAlertado: false });
+    };
 
-	const temporizadorDeReinicio = () => {
-		setCount(0);
-		setEstaAlertado(false);
-	};
-
-	return (
-		<div className="container d-flex flex-column align-items-center py-4">
-			{estaAlertado && 
-				<div className="alert alert-danger" role="alert">
-					{'Alerta!! El temporizador ha alcanzado ' + alertaEnEntrada + ' segundos.'}
-				</div>
-			}
-			<TemporizadorDeSegundos segundos={timerSeconds} />
-			
-			<div className="row justify-content-center my-3">
-				<div className="col-auto">
-					<button type="button" className="btn btn-secondary mx-2" onClick={alternarPausa}>
-						{!estaPausa ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
-					</button>
-					<button type="button" className="btn btn-secondary mx-2" onClick={temporizadorDeReinicio}>
-						<FontAwesomeIcon icon={faArrowsRotate} />
-					</button>
-				</div>
-			</div>
-			
-			<div className="row justify-content-center">
-				<div className="col-auto">
-					<div className="input-group">
-						<div className="input-group-prepend">
-							<span className="input-group-text">Alerta en</span>
-						</div>
-						<input
-							type="text"
-							className="form-control text-center"
-							aria-label="alerta en (segundos)"
-							onChange={manejarCambioEntrada}
-							value={alertaEnEntrada}
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
+    render() {
+        return (
+            <div className="container d-flex flex-column align-items-center py-4">
+                {this.state.estaAlertado && 
+                    <div className="alert alert-danger" role="alert">
+                        {'Alerta!! El temporizador ha alcanzado ' + this.state.alertaEnEntrada + ' segundos.'}
+                    </div>
+                }
+                <TemporizadorDeSegundos segundos={this.state.timerSeconds} />
+                
+                <div className="row justify-content-center my-3">
+                    <div className="col-auto">
+                        <button type="button" className="btn btn-secondary mx-2" onClick={this.alternarPausa}>
+                            {!this.state.estaPausa ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
+                        </button>
+                        <button type="button" className="btn btn-secondary mx-2" onClick={this.temporizadorDeReinicio}>
+                            <FontAwesomeIcon icon={faArrowsRotate} />
+                        </button>
+                    </div>
+                </div>
+                
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text">Alerta en</span>
+                            </div>
+                            <input
+                                type="text"
+                                className="form-control text-center"
+                                aria-label="alerta en (segundos)"
+                                onChange={this.manejarCambioEntrada}
+                                value={this.state.alertaEnEntrada}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Home;
